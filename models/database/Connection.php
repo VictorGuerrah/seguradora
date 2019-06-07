a5cf87e5ca17827f9c4d5e1c78cbd733626014d7
VictorGuerrah <victorguerrah@gmail.com>
2019-06-06 21:08:37 -0300

    Consertando database

--- models/database/Connection.php
+++ models/database/Connection.php
@@ -5,7 +5,7 @@ class Connection{
     public static function make($config){
         try{
             return new PDO(
-                $config['connection'].';dbname'.$config['name'],
+                $config['connection'].';dbname='.$config['name'],
                 $config['username'],
                 $config['password'],
                 $config['options']
