9a89ee6836b229ecb4894f1880e7517a50c9934c
VictorGuerrah <victorguerrah@gmail.com>
2019-07-01 17:44:03 -0300

    Dashboard

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -1,29 +1,61 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Index</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link href="assets/css/tipo.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
-  </head>
-  <body>
-      
-    <?php require 'views/partial/header.php'; ?>
-    
-    <hr>
-    <h1 class="centralizado">DashBoard</h1>
-    <hr>
+<head>
+  <title>Index</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <?php require 'views/partial/footer.php'; ?>
-  
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+
+  <!-- Bootstrap CSS -->
+  <link href="assets/css/tipo.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
+</head>
+
+<body>
+
+  <?php require 'views/partial/header.php'; ?>
+
+  <hr>
+  <h1 class="centralizado">Bem vindo!</h1>
+  <hr>
+
+  <div>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" id="myModalLabel">Mais sobre $funcionario->nome</h4>
+        </div>
+        <div class="modal-body">
+          <center>
+            <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" width="140" height="140" border="0" class="img-circle"></a>
+            <h3 class="media-heading">$funcionario->nome <small>$funcionario->cidade</small></h3>
+            <span><strong>Funções: </strong></span>
+            <span class="label label-warning">HTML5/CSS</span>
+            <span class="label label-info">Adobe CS 5.5</span>
+            <span class="label label-info">Microsoft Office</span>
+            <span class="label label-success">Windows XP, Vista, 7</span>
+          </center>
+          <hr>
+          <center>
+            <p class="text-left"><strong>Bio: </strong><br>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores recusandae hic omnis possimus quos minus optio doloremque dolorum rerum inventore, atque, vero veritatis facere dolore magni! Impedit soluta delectus fuga.</p>
+            <br>
+          </center>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <?php require 'views/partial/footer.php'; ?>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
